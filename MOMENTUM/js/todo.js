9eeb708fcacb11de37316c0d8ad0412da82865b5
jangjihyun <hyoni89@bythem.net>
2023-06-12 11:26:59 +0900

    [add] li 삭제 button 추가 / button click시 선택한 target.parentElement remove

--- MOMENTUM/js/todo.js
+++ MOMENTUM/js/todo.js
@@ -2,16 +2,26 @@ const $toDoForm = document.getElementById('todo-form');
 const $toDoInput = $toDoForm.querySelector('input');
 const $toDoList = document.getElementById('todo-list');
 
+function deleteToDo(event) {
+  // button click시 li 제거
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement('li');
   const span = document.createElement('span');
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement('button');
+  button.innerText = "❌";
+  button.addEventListener('click', deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   $toDoList.appendChild(li);
 }
 
-function handleToDoSubmit(e) {
-  e.preventDefault();
+function handleToDoSubmit(event) {
+  event.preventDefault();
   const newTodo = $toDoInput.value;
   $toDoInput.value = "";// enter시 input값 비우기
   paintToDo(newTodo);
